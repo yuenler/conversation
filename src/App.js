cc7eb7eb81a121cb70ad4d5c07d4c12b9ff74ffd
Yuen Ler Chow <yuenlerchow@college.harvard.edu>
2021-10-26 01:56:39 -0400

    everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+const items = ["How's your day going?", 
+"Have you done anything exciting lately?",
+"What made you smile today?",
+"What was the last good book you read?",
+"Do you have any regrets in life?",
+"Who do you look up to?",
+"Where would be your dream vacation?",
+"What kind of music do you listen to?",
+"Have you ever been in love before?",
+"What one invention needs to exist?",
+"What would be a perfect date night?",
+"What is your biggest accomplishment?",
+"What fear would you like to overcome?",
+"If you could make up a school subject, what would it be?",
+"What is your very favorite letter of the alphabet? Why?",
+"If someone gave you 20 dollars, what would you buy with it?",
+"If you had a chance to eat dessert for breakfast every day, what dessert would you choose?",
+"What is the first thing you think about when you wake up in the morning?",
+"Which animal at the zoo do you like best?",
+"If your friend is sad, what do you do to make them feel better?",
+"If you had the opportunity to invent a new ice cream flavor, what would it be?",
+"What is the silliest thing someone has ever said to you?",
+"What’s your idea of a perfect playground?",
+"What’s your favorite word?",
+"Would you switch roles with your parent if you had the chance?",
+"When was the last time you used the words 'thank you'?",
+"If you had a robot for the entire day, what would you ask it to do?",
+"Do you prefer cold weather or hot weather?",
+"If you never had to eat one vegetable, which would it be?",
+"What was the last thing that made you smile?",
+"What is your favorite part about school? Your least favorite?",
+"How do you make friends?",
+"Who makes you laugh the most?",
+"What do you most like about yourself?",
+"What hurts your feelings?",
+"If you could be famous, would you want to? Why?",
+"Who is a celebrity you admire?",
+"What made you laugh at school today?",
+"Did anything make you upset today?",
+"If you could have more friends, would you?",
+"What do you like most about your friends?",
+"Is there anyone at school you’d like to know better?",
+"Have you ever lost a friend? Why?",
+"Where do you want to be ten years from now?",
+"If you had $100, what would you spend it on?",
+"Is there something you’d want to do as a family?",
+"If you were to choose one way to be disciplined, what would it be?",
+"What do you think are the best traits for a person to have?",
+"Would you ever get a tattoo? What would it be?",
+"What do you think is a good age to start dating?",
+"Why do you think popular kids are popular?",
+"If you could go anywhere in the world, where would you choose and why?",
+"What is something you wish you could do everyday?",
+"What are the top three things on your bucket list?",
+"How do you think you will die?",
+"What has been the lowest point of your life?",
+"If you could ask for a miracle, what would it be?",
+"Where do you see yourself in five years?",
+"What is the biggest risk you’ve ever taken?",
+"What would your ideal life look like?",
+"If someone gave you an envelope with your death date inside of it, would you open it?",
+"When have you been the most happy?",
+"Do you know anyone who is living their life to the fullest?",
+"What is your idea of the perfect day?",
+"Who has been the most influential person in your life and why?",
+"What book had a big influence on you?",
+"Do you think your priorities have changed since you were younger?",
+"What is the most memorable lesson you learned from your parents?",
+"What does success mean to you?",
+"What is the most difficult thing you’ve ever done?",
+"What scares you most about your future?",
+"What keeps you up at night?",
+"What’s something not many people know about you?",
+"What are you most passionate about?",
+"What makes you laugh out loud?",
+"What was your favorite thing to do as a kid?",
+"Who do you text the most?",
+"What do you like to cook the most?",
+"What’s your favorite TV show?",
+"What is your favorite book?",
+"Who is most influential in your life?",
+"What was your best friend’s name growing up?",
+"How do you spend your mornings?",
+"What’s your all-time favorite band?",
+"What’s your dream job?",
+"What did you study?",
+"What did you want to be growing up?",
+"What is the best pickup line you’ve ever used? Heard?",
+"Do you have any nicknames?",
+"What talent do you wish you had?",
+"Where do you see yourself living when you retire?",
+"What is your favorite weekend activity?",
+"Do you have any pet peeves?",
+"Who was your favorite teacher and why?",
+"What makes you most uncomfortable about dating?",
+"What is your favorite place in the entire world?",
+]
 
 function App() {
+  const [question, setQuestion] = useState("Click the Generate button if the conversation starts to die.");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#12109e",
+      color: "white",
+      height: '100vh',
+      textAlign: 'center'
+    }}>
+      <div>
+        <button className="button" onClick={() => setQuestion(items[Math.floor(Math.random()*items.length)])}>Generate</button>
+        <p style={{fontSize: 30}}>{question}</p>
+      </div>
     </div>
   );
 }
